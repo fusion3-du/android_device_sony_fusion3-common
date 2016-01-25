ae786878daa555e1d2d92310a85e25f610a00ed7
BrateloSlava <d_slava@list.ru>
2016-01-25 21:02:19 +0100

    Tell dumpvar to report correct kernel TC
    Change-Id: Ie990d12204e3383c837f2a3d20b5fd60bdacedfc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,6 +48,7 @@ BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000
 TARGET_KERNEL_SOURCE  := kernel/sony/apq8064
 
  # Kernel Toolchain
+TARGET_GCC_VERSION_ARM := 5.3
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.3/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
