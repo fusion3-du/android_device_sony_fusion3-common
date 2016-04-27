354dbcdc91fe8cae40e74fea95f2ed158d025bab
airlessproject <vlatko.mitreski@gmail.com>
2016-04-27 04:56:26 +0200

    set read-ahead values
    for both internal and external sd

---
+++ rootdir/system/etc/set_read_ahead.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+
+### Internal SD read ahead
+MMC=`ls -d /sys/block/mmcblk*`
+for i in $MMC; do
+	if [ -e $i/bdi/read_ahead_kb ]; then
+		echo "512" > $i/bdi/read_ahead_kb
+	fi;
+done
+
+### SDcard read ahead
+VD=`ls -d /sys/devices/virtual/bdi/*`
+for i in $VD; do
+	echo "512" > $i/read_ahead_kb;
+done
