61bbc9436bf8d21cea99e7dd28d1884d935b35b4
airlessproject <vlatko.mitreski@gmail.com>
2016-05-17 03:45:14 +0200

    init changes
    Change-Id: Ia2eb3776ef5eda1e7457c5f042476e74b49bbf88

--- rootdir/system/etc/set_custom_sysfs_values.sh
+++ rootdir/system/etc/set_custom_sysfs_values.sh
@@ -16,6 +16,3 @@ done
 
 ### Multicore Power Saving
 echo "1" > /sys/devices/system/cpu/sched_mc_power_savings
-
-### Dt2w
-echo "0" > /sys/devices/virtual/input/clearpad/wakeup_gesture
---
+++ rootdir/system/etc/set_custom_sysfs_values_boot.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+
+### Disable Dt2w
+echo "0" > /sys/devices/virtual/input/clearpad/wakeup_gesture
