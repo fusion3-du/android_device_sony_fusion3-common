7141c5539472747958df4c09c55b4f6623fad1f2
airlessproject <vlatko.mitreski@gmail.com>
2016-07-02 22:10:58 +0200

    init changes

--- rootdir/system/etc/set_custom_sysfs_values.sh
+++ rootdir/system/etc/set_custom_sysfs_values.sh
@@ -8,11 +8,11 @@ for i in $MMC; do
 	fi;
 done
 
-### SDcard read ahead
+### Virtual SD read ahead
 VD=`ls -d /sys/devices/virtual/bdi/*`
 for i in $VD; do
 	echo "512" > $i/read_ahead_kb;
 done
 
-### Multicore Power Saving
-echo "1" > /sys/devices/system/cpu/sched_mc_power_savings
+### External SD read ahead
+echo "4096" > /sys/block/mmcblk1/bdi/read_ahead_kb
