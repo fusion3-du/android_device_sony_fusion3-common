6a04832080d18ea69fe45e83fc2568e8d0ab733b
airlessproject <vlatko.mitreski@gmail.com>
2016-07-13 14:54:15 +0200

    init changes

--- rootdir/system/etc/set_custom_sysfs_values_boot.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-
-### Disable Dt2w
-echo "0" > /sys/devices/virtual/input/clearpad/wakeup_gesture
-
-### Multicore Power Saving
-echo "1" > /sys/devices/system/cpu/sched_mc_power_savings
